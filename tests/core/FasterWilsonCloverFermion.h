57bceb765051a44e447055d1284eebb16b5bcb89
Daniel Richtmann <daniel.richtmann@gmail.com>
2020-12-04 17:57:40 +0100

    Check in first version of faster clover term

---
+++ tests/core/FasterWilsonCloverFermion.h
@@ -0,0 +1,348 @@
+/*************************************************************************************
+
+    Grid physics library, www.github.com/paboyle/Grid
+
+    Source file: ./tests/core/FasterWilsonCloverFermion.h
+
+    Copyright (C) 2020
+
+    Author: Daniel Richtmann <daniel.richtmann@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See the full license in the file "LICENSE" in the top level distribution directory
+    *************************************************************************************/
+/*  END LEGAL */
+
+#pragma once
+
+#include <Grid/Grid.h>
+
+// see Grid/qcd/action/fermion/WilsonCloverFermion for description
+
+// Modifications done here:
+//
+// Grid: clover term = 12x12 matrix per site
+//
+// But: Only two diagonal 6x6 hermitian blocks are non-zero (also true in Grid, verified by running)
+// Sufficient to store/transfer only the real parts of the diagonal and one triangular part
+// 2 * (6 + 15 * 2) = 72 real or 36 complex words to be stored/transfered
+//
+// Here: Above but diagonal as complex numbers, i.e., need to store/transfer
+// 2 * (6 * 2 + 15 * 2) = 84 real or 42 complex words
+//
+// Words per site and improvement compared to Grid (combined with the input and output spinors:
+//
+// - Grid:    2*12 + 12*12 = 168 words -> 1.00 x less
+// - Minimal: 2*12 + 36    =  60 words -> 2.80 x less
+// - Here:    2*12 + 42    =  66 words -> 2.55 x less
+//
+// These improvements directly translate to wall-clock time
+
+NAMESPACE_BEGIN(Grid);
+
+template<class Impl>
+class FasterWilsonCloverFermion : public WilsonCloverFermion<Impl> {
+  /////////////////////////////////////////////
+  // Sizes
+  /////////////////////////////////////////////
+
+public:
+
+  static constexpr int Nred      = Nc * Nhs;
+  static constexpr int Nblock    = Nhs;
+  static constexpr int Ndiag     = Nred;
+  static constexpr int Ntriangle = (Nred - 1) * Nc;
+
+  /////////////////////////////////////////////
+  // Type definitions
+  /////////////////////////////////////////////
+
+public:
+
+  static_assert(Nd == 4 && Nc == 3 && Ns == 4 && Impl::Dimension == 3, "Wrong dimensions");
+  INHERIT_IMPL_TYPES(Impl);
+
+  template<typename vtype> using iImplCloverDiagonal = iScalar<iVector<iVector<vtype,  6>, 2>>;
+  template<typename vtype> using iImplCloverTriangle = iScalar<iVector<iVector<vtype, 15>, 2>>;
+
+  typedef iImplCloverDiagonal<Simd> SiteCloverDiagonal;
+  typedef iImplCloverTriangle<Simd> SiteCloverTriangle;
+
+  typedef Lattice<SiteCloverDiagonal> CloverDiagonalField;
+  typedef Lattice<SiteCloverTriangle> CloverTriangleField;
+
+  typedef WilsonCloverFermion<Impl> WilsonCloverBase;
+  typedef typename WilsonCloverBase::SiteCloverType SiteClover;
+  typedef typename WilsonCloverBase::CloverFieldType CloverField;
+
+  /////////////////////////////////////////////
+  // Member Functions
+  /////////////////////////////////////////////
+
+public:
+
+  FasterWilsonCloverFermion(GaugeField& _Umu,
+                            GridCartesian& Fgrid,
+                            GridRedBlackCartesian& Hgrid,
+                            const RealD _mass,
+                            const RealD _csw_r = 0.0,
+                            const RealD _csw_t = 0.0,
+                            const WilsonAnisotropyCoefficients& clover_anisotropy = WilsonAnisotropyCoefficients(),
+                            const ImplParams& impl_p = ImplParams())
+    : WilsonCloverFermion<Impl>(_Umu, Fgrid, Hgrid, _mass, _csw_r, _csw_t, clover_anisotropy, impl_p)
+    , Diag(&Fgrid),           Triangle(&Fgrid)
+    , DiagEven(&Hgrid),       TriangleEven(&Hgrid)
+    , DiagOdd(&Hgrid),        TriangleOdd(&Hgrid)
+    , DiagInv(&Fgrid),        TriangleInv(&Fgrid)
+    , DiagInvEven(&Hgrid),    TriangleInvEven(&Hgrid)
+    , DiagInvOdd(&Hgrid),     TriangleInvOdd(&Hgrid)
+    , DiagDag(&Fgrid),        TriangleDag(&Fgrid)
+    , DiagDagEven(&Hgrid),    TriangleDagEven(&Hgrid)
+    , DiagDagOdd(&Hgrid),     TriangleDagOdd(&Hgrid)
+    , DiagInvDag(&Fgrid),     TriangleInvDag(&Fgrid)
+    , DiagInvDagEven(&Hgrid), TriangleInvDagEven(&Hgrid)
+    , DiagInvDagOdd(&Hgrid),  TriangleInvDagOdd(&Hgrid)
+  {
+    double t0 = usecond();
+    convertLayout(this->CloverTerm, Diag, Triangle);
+    convertLayout(this->CloverTermEven, DiagEven, TriangleEven);
+    convertLayout(this->CloverTermOdd, DiagOdd, TriangleOdd);
+
+    convertLayout(this->CloverTermInv, DiagInv, TriangleInv);
+    convertLayout(this->CloverTermInvEven, DiagInvEven, TriangleInvEven);
+    convertLayout(this->CloverTermInvOdd, DiagInvOdd, TriangleInvOdd);
+
+    convertLayout(closure(adj(this->CloverTerm)), DiagDag, TriangleDag);
+    convertLayout(this->CloverTermDagEven, DiagDagEven, TriangleDagEven);
+    convertLayout(this->CloverTermDagOdd, DiagDagOdd, TriangleDagOdd);
+
+    convertLayout(closure(adj(this->CloverTermInv)), DiagInvDag, TriangleInvDag);
+    convertLayout(this->CloverTermInvDagEven, DiagInvDagEven, TriangleInvDagEven);
+    convertLayout(this->CloverTermInvDagOdd, DiagInvDagOdd, TriangleInvDagOdd);
+
+    // TODO: set original clover fields to zero size
+    // BUT:  keep them around if we want to apply MDeriv
+    double t1 = usecond();
+    std::cout << GridLogMessage << "FasterWilsonCloverFermion: layout conversions took " << (t1-t0)/1e6 << " seconds" << std::endl;
+  }
+
+
+  void convertLayout(const CloverField& full, CloverDiagonalField& diag, CloverTriangleField& triangle) {
+    conformable(full.Grid(), diag.Grid());
+    conformable(full.Grid(), triangle.Grid());
+
+    diag.Checkerboard()     = full.Checkerboard();
+    triangle.Checkerboard() = full.Checkerboard();
+
+    autoView(full_v, full, AcceleratorRead);
+    autoView(diag_v, diag, AcceleratorWrite);
+    autoView(triangle_v, triangle, AcceleratorWrite);
+
+    // NOTE: this function cannot be 'private' since nvcc forbids this for kernels
+    accelerator_for(ss, full.Grid()->oSites(), 1, {
+      for(int s_row = 0; s_row < Ns; s_row++) {
+        for(int s_col = 0; s_col < Ns; s_col++) {
+          if(abs(s_row - s_col) > 1 || s_row + s_col == 3) continue;
+          int block       = s_row / Nhs;
+          int s_row_block = s_row % Nhs;
+          int s_col_block = s_col % Nhs;
+          for(int c_row = 0; c_row < Nc; c_row++) {
+            for(int c_col = 0; c_col < Nc; c_col++) {
+              int i = s_row_block * Nc + c_row;
+              int j = s_col_block * Nc + c_col;
+              if(i == j)
+                diag_v[ss]()(block)(i) = full_v[ss]()(s_row, s_col)(c_row, c_col);
+              else if(i < j)
+                triangle_v[ss]()(block)(triangle_index(i, j)) = full_v[ss]()(s_row, s_col)(c_row, c_col);
+              else
+                continue;
+            }
+          }
+        }
+      }
+    });
+  }
+
+
+  void MooeeInternal(const FermionField& in, FermionField& out, int dag, int inv) override {
+    conformable(in.Grid(), out.Grid());
+
+    assert(in.Checkerboard() == Odd || in.Checkerboard() == Even);
+    out.Checkerboard() = in.Checkerboard();
+
+    CloverDiagonalField* diag;
+    CloverTriangleField* triangle;
+
+    // decision making from original implementation
+    if(dag) {
+      if(in.Grid()->_isCheckerBoarded) {
+        if(in.Checkerboard() == Odd) {
+          diag     = (inv) ? &DiagInvDagOdd :     &DiagDagOdd;
+          triangle = (inv) ? &TriangleInvDagOdd : &TriangleDagOdd;
+        } else {
+          diag     = (inv) ? &DiagInvDagEven :     &DiagDagEven;
+          triangle = (inv) ? &TriangleInvDagEven : &TriangleDagEven;
+        }
+      } else {
+        diag     = (inv) ? &DiagInvDag :     &DiagDag;
+        triangle = (inv) ? &TriangleInvDag : &TriangleDag;
+      }
+    } else {
+      if(in.Grid()->_isCheckerBoarded) {
+        if(in.Checkerboard() == Odd) {
+          diag     = (inv) ? &DiagInvOdd :     &DiagOdd;
+          triangle = (inv) ? &TriangleInvOdd : &TriangleOdd;
+        } else {
+          diag     = (inv) ? &DiagInvEven :     &DiagEven;
+          triangle = (inv) ? &TriangleInvEven : &TriangleEven;
+        }
+      } else {
+        diag     = (inv) ? &DiagInv :     &Diag;
+        triangle = (inv) ? &TriangleInv : &Triangle;
+      }
+    }
+
+    conformable(diag->Grid(), in.Grid());
+    conformable(triangle->Grid(), in.Grid());
+
+    MooeeKernel(in, out, *diag, *triangle);
+  }
+
+
+  void MooeeKernel(const FermionField&        in,
+                   FermionField&              out,
+                   const CloverDiagonalField& diag,
+                   const CloverTriangleField& triangle) {
+    // #if defined(GRID_CUDA) || defined(GRID_HIP)
+    MooeeKernel_gpu(in, out, diag, triangle);
+    // #else
+    //     MooeeKernel_cpu(in, out, diag, triangle);
+    // #endif
+  }
+
+
+  void MooeeKernel_gpu(const FermionField&        in,
+                       FermionField&              out,
+                       const CloverDiagonalField& diag,
+                       const CloverTriangleField& triangle) {
+    autoView(diag_v,     diag,     AcceleratorRead);
+    autoView(triangle_v, triangle, AcceleratorRead);
+    autoView(in_v,       in,       AcceleratorRead);
+    autoView(out_v,      out,      AcceleratorWrite);
+
+    typedef decltype(coalescedRead(out_v[0])) CalcSpinor;
+    const uint64_t Nsite = diag.Grid()->oSites();
+
+    accelerator_for(ss, Nsite, Simd::Nsimd(), {
+      CalcSpinor res;
+      CalcSpinor in_t = in_v(ss);
+      auto diag_t = diag_v(ss);
+      auto triangle_t = triangle_v(ss);
+      for(int block=0; block<Nhs; block++) {
+        int s_start = block*Nhs;
+        for(int i=0; i<Nred; i++) {
+          int si = s_start + i/Nc, ci = i%Nc;
+          res()(si)(ci) = diag_t()(block)(i) * in_t()(si)(ci);
+          for(int j=0; j<Nred; j++) {
+            if (j == i) continue;
+            int sj = s_start + j/Nc, cj = j%Nc;
+            res()(si)(ci) = res()(si)(ci)+ triangle_elem(triangle_t, block, i, j) * in_t()(sj)(cj);
+          };
+        };
+      };
+      coalescedWrite(out_v[ss], res);
+    });
+  }
+
+
+  void MooeeKernel_cpu(const FermionField&        in,
+                       FermionField&              out,
+                       const CloverDiagonalField& diag,
+                       const CloverTriangleField& triangle) {
+    autoView(diag_v,     diag,     CpuRead);
+    autoView(triangle_v, triangle, CpuRead);
+    autoView(in_v,       in,       CpuRead);
+    autoView(out_v,      out,      CpuWrite);
+
+    typedef SiteSpinor CalcSpinor;
+    const uint64_t Nsite = diag.Grid()->oSites();
+
+    // TODO: Add CPU code
+    thread_for(ss, Nsite, Simd::Nsimd(), {
+      CalcSpinor res;
+      CalcSpinor in_t = in_v[ss];
+      auto diag_t = diag_v[ss];
+      auto triangle_t = triangle_v[ss];
+
+      for(int block=0; block<Nhs; block++) {
+        int s_start = block*Nhs;
+        for(int i=0; i<Nred; i++) {
+          int si = s_start + i/Nc, ci = i%Nc;
+          res()(si)(ci) = diag_t()(block)(i) * in_t()(si)(ci);
+          for(int j=0; j<Nred; j++) {
+            if (j == i) continue;
+            int sj = s_start + j/Nc, cj = j%Nc;
+            res()(si)(ci) = res()(si)(ci)+ triangle_elem(triangle_t, block, i, j) * in_t()(sj)(cj);
+          };
+        };
+      };
+      coalescedWrite(out_v[ss], res);
+    });
+  }
+
+  /////////////////////////////////////////////
+  // Helpers
+  /////////////////////////////////////////////
+
+private:
+
+  template<typename vobj>
+  accelerator_inline vobj triangle_elem(const iImplCloverTriangle<vobj>& triang, int block, int i, int j) {
+    assert(i != j);
+    if(i < j) {
+      return triang()(block)(triangle_index(i, j));
+    } else { // i > j
+      return conjugate(triang()(block)(triangle_index(i, j)));
+    }
+  }
+
+  accelerator_inline int triangle_index(int i, int j) {
+    if(i == j)
+      return 0;
+    else if(i < j)
+      return Nred * (Nred - 1) / 2 - (Nred - i) * (Nred - i - 1) / 2 + j - i - 1;
+    else // i > j
+      return Nred * (Nred - 1) / 2 - (Nred - j) * (Nred - j - 1) / 2 + i - j - 1;
+  }
+
+  /////////////////////////////////////////////
+  // Member Data
+  /////////////////////////////////////////////
+
+private:
+
+  CloverDiagonalField Diag,       DiagEven,       DiagOdd;
+  CloverDiagonalField DiagInv,    DiagInvEven,    DiagInvOdd;
+  CloverDiagonalField DiagDag,    DiagDagEven,    DiagDagOdd;
+  CloverDiagonalField DiagInvDag, DiagInvDagEven, DiagInvDagOdd;
+
+  CloverTriangleField Triangle,       TriangleEven,       TriangleOdd;
+  CloverTriangleField TriangleInv,    TriangleInvEven,    TriangleInvOdd;
+  CloverTriangleField TriangleDag,    TriangleDagEven,    TriangleDagOdd;
+  CloverTriangleField TriangleInvDag, TriangleInvDagEven, TriangleInvDagOdd;
+};
+
+NAMESPACE_END(Grid);
---
+++ tests/core/Test_clover_speedup_foundation.cc
@@ -0,0 +1,184 @@
+/*************************************************************************************
+
+    Grid physics library, www.github.com/paboyle/Grid
+
+    Source file: ./tests/core/Test_clover_speedup_foundation.cc
+
+    Copyright (C) 2015 - 2020
+
+    Author: Daniel Richtmann <daniel.richtmann@gmail.com>
+            Nils Meyer       <nils.meyer@ur.de>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See the full license in the file "LICENSE" in the top level distribution directory
+*************************************************************************************/
+/*  END LEGAL */
+
+#include <Grid/Grid.h>
+#include "FasterWilsonCloverFermion.h"
+
+using namespace Grid;
+
+
+int readFromCommandLineInt(int* argc, char*** argv, const std::string& option, int defaultValue) {
+  std::string arg;
+  int         ret = defaultValue;
+  if(GridCmdOptionExists(*argv, *argv + *argc, option)) {
+    arg = GridCmdOptionPayload(*argv, *argv + *argc, option);
+    GridCmdOptionInt(arg, ret);
+  }
+  return ret;
+}
+
+
+#define grid_printf(...) \
+{\
+  char _buf[1024];\
+  sprintf(_buf, __VA_ARGS__);\
+  std::cout << GridLogMessage << _buf;\
+  fflush(stdout);\
+}
+
+
+template<typename Field>
+bool resultsAgree(const Field& ref, const Field& res, const std::string& name) {
+  RealD checkTolerance = (getPrecision<Field>::value == 1) ? 1e-7 : 1e-15;
+  Field diff(ref.Grid());
+  diff = ref - res;
+  auto absDev = norm2(diff);
+  auto relDev = absDev / norm2(ref);
+  std::cout << GridLogMessage
+            << "norm2(reference), norm2(" << name << "), abs. deviation, rel. deviation: " << norm2(ref) << " "
+            << norm2(res) << " " << absDev << " " << relDev << " -> check "
+            << ((relDev < checkTolerance) ? "passed" : "failed") << std::endl;
+
+  return relDev <= checkTolerance;
+}
+
+
+template<typename vCoeff_t>
+void runBenchmark(int* argc, char*** argv) {
+  // precision
+  static_assert(getPrecision<vCoeff_t>::value == 2 || getPrecision<vCoeff_t>::value == 1, "Incorrect precision"); // double or single
+  std::string precision = (getPrecision<vCoeff_t>::value == 2 ? "double" : "single");
+
+  // setup grids
+  GridCartesian*         UGrid   = SpaceTimeGrid::makeFourDimGrid(GridDefaultLatt(), GridDefaultSimd(Nd, vCoeff_t::Nsimd()), GridDefaultMpi());
+  GridRedBlackCartesian* UrbGrid = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid);
+  // clang-format on
+
+  // setup rng
+  std::vector<int> seeds({1, 2, 3, 4});
+  GridParallelRNG  pRNG(UGrid);
+  pRNG.SeedFixedIntegers(seeds);
+
+  // type definitions
+  typedef WilsonImpl<vCoeff_t, FundamentalRepresentation, CoeffReal> WImpl;
+  typedef WilsonCloverFermion<WImpl> WilsonCloverOperator;
+  typedef FasterWilsonCloverFermion<WImpl> FasterWilsonCloverOperator;
+  typedef typename WilsonCloverOperator::FermionField Fermion;
+  typedef typename WilsonCloverOperator::GaugeField Gauge;
+
+  // setup fields
+  Fermion src(UGrid); random(pRNG, src);
+  Fermion ref(UGrid); ref = Zero();
+  Fermion res(UGrid); res = Zero();
+  Fermion hop(UGrid); hop = Zero();
+  Fermion diff(UGrid); diff = Zero();
+  Gauge   Umu(UGrid); SU3::HotConfiguration(pRNG, Umu);
+
+  // setup boundary phases
+  typename WilsonCloverOperator::ImplParams implParams;
+  std::vector<Complex> boundary_phases(Nd, 1.);
+  if(GridCmdOptionExists(*argv, *argv + *argc, "--antiperiodic")) boundary_phases[Nd-1] = -1.;
+  implParams.boundary_phases = boundary_phases;
+  WilsonAnisotropyCoefficients anisParams;
+
+  // misc stuff needed for benchmarks
+  const int nIter = readFromCommandLineInt(argc, argv, "--niter", 1000);
+  double volume=1.0; for(int mu=0; mu<Nd; mu++) volume*=UGrid->_fdimensions[mu];
+
+  // setup fermion operators
+  WilsonCloverOperator     Dwc(Umu, *UGrid, *UrbGrid, 0.5, 1.0, 1.0, anisParams, implParams);
+  FasterWilsonCloverOperator Dwc_faster(Umu, *UGrid, *UrbGrid, 0.5, 1.0, 1.0, anisParams, implParams);
+
+  // warmup + measure dhop
+  grid_printf("hop warmup %s\n", precision.c_str());
+  for(auto n : {1, 2, 3, 4, 5}) Dwc.Dhop(src, hop, 0);
+  grid_printf("hop measurement %s\n", precision.c_str());
+  double t0 = usecond();
+  for(int n = 0; n < nIter; n++) Dwc.Dhop(src, hop, 0);
+  double t1 = usecond();
+  double secs_hop = (t1-t0)/1e6;
+
+  // warmup + measure reference clover
+  grid_printf("reference warmup %s\n", precision.c_str());
+  for(auto n : {1, 2, 3, 4, 5}) Dwc.Mooee(src, ref);
+  grid_printf("reference measurement %s\n", precision.c_str());
+  double t2 = usecond();
+  for(int n = 0; n < nIter; n++) Dwc.Mooee(src, ref);
+  double t3 = usecond();
+  double secs_ref = (t3-t2)/1e6;
+
+  // warmup + measure improved clover
+  grid_printf("improved warmup %s\n", precision.c_str());
+  for(auto n : {1, 2, 3, 4, 5}) Dwc_faster.Mooee(src, res);
+  grid_printf("improved measurement %s\n", precision.c_str());
+  double t4 = usecond();
+  for(int n = 0; n < nIter; n++) Dwc_faster.Mooee(src, res);
+  double t5 = usecond();
+  assert(resultsAgree(ref, res, "improved"));
+  double secs_res = (t5-t4)/1e6;
+
+  // performance per site (use minimal values necessary)
+  double hop_flop_per_site            = 1320; // Rich's Talk + what Peter uses
+  double hop_byte_per_site            = (8 * 9 + 9 * 12) * 2 * getPrecision<vCoeff_t>::value * 4;
+  double clov_flop_per_site           = 504; // Rich's Talk and 1412.2629
+  double clov_byte_per_site           = (2 * 18 + 12 + 12) * 2 * getPrecision<vCoeff_t>::value * 4;
+  double clov_flop_per_site_performed = 1128;
+  double clov_byte_per_site_performed = (12 * 12 + 12 + 12) * 2 * getPrecision<vCoeff_t>::value * 4;
+
+  // total performance numbers
+  double hop_gflop_total            = volume * nIter * hop_flop_per_site / 1e9;
+  double hop_gbyte_total            = volume * nIter * hop_byte_per_site / 1e9;
+  double clov_gflop_total           = volume * nIter * clov_flop_per_site / 1e9;
+  double clov_gbyte_total           = volume * nIter * clov_byte_per_site / 1e9;
+  double clov_gflop_performed_total = volume * nIter * clov_flop_per_site_performed / 1e9;
+  double clov_gbyte_performed_total = volume * nIter * clov_byte_per_site_performed / 1e9;
+
+  // output
+  grid_printf("Performance(%35s, %s): %2.4f s, %6.0f GFlop/s, %6.0f GByte/s, speedup vs ref = %.2f, fraction of hop = %.2f\n",
+              "hop", precision.c_str(), secs_hop, hop_gflop_total/secs_hop, hop_gbyte_total/secs_hop, secs_ref/secs_hop, secs_hop/secs_hop);
+  grid_printf("Performance(%35s, %s): %2.4f s, %6.0f GFlop/s, %6.0f GByte/s, speedup vs ref = %.2f, fraction of hop = %.2f\n",
+              "reference", precision.c_str(), secs_ref, clov_gflop_total/secs_ref, clov_gbyte_total/secs_ref, secs_ref/secs_ref, secs_ref/secs_hop);
+  grid_printf("Performance(%35s, %s): %2.4f s, %6.0f GFlop/s, %6.0f GByte/s, speedup vs ref = %.2f, fraction of hop = %.2f\n",
+              "improved", precision.c_str(), secs_res, clov_gflop_total/secs_res, clov_gbyte_total/secs_res, secs_ref/secs_res, secs_res/secs_hop);
+
+  // just so we see how well the ET performs in terms of traffic
+  grid_printf("Performance(%35s, %s): %2.4f s, %6.0f GFlop/s, %6.0f GByte/s, speedup vs ref = %.2f, fraction of hop = %.2f\n",
+              "reference_performed", precision.c_str(), secs_ref, clov_gflop_performed_total/secs_ref, clov_gbyte_performed_total/secs_ref, secs_ref/secs_ref, secs_ref/secs_hop);
+
+  grid_printf("finalize %s\n", precision.c_str());
+}
+
+int main(int argc, char** argv) {
+  Grid_init(&argc, &argv);
+
+  runBenchmark<vComplexD>(&argc, &argv);
+  runBenchmark<vComplexF>(&argc, &argv);
+
+  Grid_finalize();
+}
